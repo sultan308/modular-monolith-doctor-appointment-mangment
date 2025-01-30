5d70cdf8418ba9aa5b35093e82c68c44c3160ca9
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-01-30 19:39:42 +0000

    Add methods for creating and retrieving patient appointments
    Reintroduced and implemented `create_patient_appointment` and `get_all_patient_appointments` in the repository trait. These changes allow managing patient appointments, including creation and retrieval, enabling better functionality for appointment handling.

--- appointment_booking/src/domain/repository_traits/patient_appointment_repository_traits.rs
+++ appointment_booking/src/domain/repository_traits/patient_appointment_repository_traits.rs
@@ -1,12 +1,14 @@
 use crate::domain::entities::{PatientAppointmentEntity, SlotEntity, PatientEntity};
 use anyhow::Result;
 use async_trait::async_trait;
+use bson::oid::ObjectId;
 
 pub type PatientAppointmentRepositoryResult<T> = Result<T>;
 
 #[async_trait]
 pub trait PatientAppointmentRepositoryTrait: Send + Sync {
     async fn get_all_bookable_slots(&self) -> PatientAppointmentRepositoryResult<Vec<SlotEntity>>;
-    // async fn create_patient_appointment(&mut self, patient: PatientEntity, bookable_slot: SlotEntity) -> PatientAppointmentRepositoryResult<PatientAppointmentEntity>;
-    // async fn get_all_patient_appointments(&self, patient: PatientEntity) -> PatientAppointmentRepositoryResult<Vec<PatientAppointmentEntity>>;
+
+    async fn create_patient_appointment(&mut self, patient: PatientEntity, bookable_slot_id: ObjectId ) -> PatientAppointmentRepositoryResult<PatientAppointmentEntity>;
+    async fn get_all_patient_appointments(&self, patient: PatientEntity) -> PatientAppointmentRepositoryResult<Vec<PatientAppointmentEntity>>;
 }
