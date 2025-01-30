5b1f5b8cce433b6051029c85e2395fde12f3e663
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-01-30 19:40:12 +0000

    Add methods for creating and retrieving patient appointments to DoctorAvailabilityPatientAppointmentRepository
    Added `create_patient_appointment` for booking slots and `get_all_patient_appointments` for fetching all appointments for a patient. These enhancements improve the repository's functionality for handling patient appointment data.

--- appointment_booking/src/infrastructure/repositories/patient_appointment_repository.rs
+++ appointment_booking/src/infrastructure/repositories/patient_appointment_repository.rs
@@ -38,6 +38,21 @@ impl PatientAppointmentRepositoryTrait for DoctorAvailabilityPatientAppointmentR
         Ok(bookable_slots.into_iter().map(SlotEntity::from_doctor_availability_response_slot).collect())
     }
 
+    async fn create_patient_appointment(&mut self, patient: PatientEntity, bookable_slot_id: ObjectId) -> PatientAppointmentRepositoryResult<PatientAppointmentEntity> {
+        let mut slots_controller = self.slots_controller.lock().await;
+        let reserved_slot = slots_controller.reserve_slot(bookable_slot_id, patient.get_id()).await?;
+        let reserved_slot_entity = SlotEntity::from_doctor_availability_response_slot(reserved_slot);
+        Ok(PatientAppointmentEntity::new(patient, reserved_slot_entity))
+    }
+
+    async fn get_all_patient_appointments(&self, patient: PatientEntity) -> PatientAppointmentRepositoryResult<Vec<PatientAppointmentEntity>> {
+
+        let slots_controller = self.slots_controller.lock().await;
+        let patient_slots = slots_controller.get_all_slots_by_patient(patient.get_id()).await?;
+        Ok(patient_slots.into_iter()
+            .map(|patient_res_slot|PatientAppointmentEntity::new(patient.clone(),SlotEntity::from_doctor_availability_response_slot(patient_res_slot)))
+            .collect())
+    }
 }
 impl SlotEntity{
     fn convert_to_optional_date_time(opt_chrono_date_time: Option<chrono::DateTime<chrono::Utc>>) -> Option<DateTime> {
