ca333a4a2661f82600683b768d6bd468507bfaa7
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-01-30 19:41:04 +0000

    Add `BookPatientAppointmentUseCase` implementation
    Introduce the `BookPatientAppointmentUseCase` to handle booking patient appointments. This includes the creation of patient appointments and integration with related repositories and controllers. Updated exports and response handling to support the new use case.

---
+++ appointment_booking/src/usecases/book_patient_appointment_use_case.rs
@@ -0,0 +1,33 @@
+use std::sync::Arc;
+use anyhow::Result;
+use bson::oid::ObjectId;
+use doctor_availability::controllers::SlotsController;
+use futures::lock::Mutex;
+use crate::domain::{PatientAppointmentEntity, PatientAppointmentRepositoryTrait, PatientEntity};
+use crate::infrastructure::{DoctorAvailabilityPatientAppointmentRepository, MongoDataBase, MongoPatientRepository};
+use crate::usecases::GetPatientUseCase;
+use crate::usecases::responses::PatientAppointmentResponse;
+
+
+pub struct BookPatientAppointmentUseCase{
+    patient_appointment_repository: Box<dyn PatientAppointmentRepositoryTrait>,
+    get_patient_use_case: GetPatientUseCase
+}
+
+impl BookPatientAppointmentUseCase{
+    pub fn with_slots_controller_and_mongo_db(slots_controller: Arc<Mutex<SlotsController>>,
+                                              m_db: &MongoDataBase) -> BookPatientAppointmentUseCase {
+        BookPatientAppointmentUseCase {
+            patient_appointment_repository: Box::new(DoctorAvailabilityPatientAppointmentRepository::new(slots_controller)),
+            get_patient_use_case: GetPatientUseCase::with_mongo_db(m_db)
+        }
+    }
+}
+
+impl BookPatientAppointmentUseCase {
+    pub async fn invoke(&mut self, patient_id: ObjectId, slot_id: ObjectId) -> Result<PatientAppointmentResponse>{
+        let patient = self.get_patient_use_case.by_id(patient_id).await?;
+        let booked_patient_appointment = self.patient_appointment_repository.create_patient_appointment(patient.to_patient_entity(), slot_id).await?;
+        Ok(PatientAppointmentResponse::from(booked_patient_appointment))
+    }
+}
--- appointment_booking/src/usecases/responses.rs
+++ appointment_booking/src/usecases/responses.rs
@@ -1,5 +1,7 @@
 mod patient_response;
 mod available_slot_response;
+mod patient_appointment_response;
 
-pub use patient_response::ResponsePatient;
 pub use available_slot_response::AvailableSlotResponse;
+pub use patient_appointment_response::PatientAppointmentResponse;
+pub use patient_response::ResponsePatient;
