ccc7f0e88fee7c3b1d8c866e54cd9e9b3ef3e70c
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-01-30 19:41:19 +0000

    Add booking functionality to AppointmentsController
    Introduced support for booking patient appointments by integrating BookPatientAppointmentUseCase. Updated `AppointmentsController` to include methods for handling appointment bookings and retrieving patient-specific booked appointments. Enhanced constructor to accept MongoDB dependency for persistence.

--- appointment_booking/src/presentation/controllers/appointments_controller.rs
+++ appointment_booking/src/presentation/controllers/appointments_controller.rs
@@ -4,17 +4,22 @@ use futures::lock::Mutex;
 use std::sync::Arc;
 
 use doctor_availability::controllers::SlotsController;
-
-use crate::usecases::{GetAvailableSlotsUseCase,AvailableSlotResponse};
-use crate::presentation::payloads::patient_payloads::CreatePatientPayload;
+use crate::infrastructure::MongoDataBase;
+use crate::usecases::{GetAvailableSlotsUseCase,
+                      BookPatientAppointmentUseCase,
+                      PatientAppointmentResponse,
+                      AvailableSlotResponse};
 
 pub struct AppointmentsController {
     get_available_slots_use_case: GetAvailableSlotsUseCase,
+    book_patient_appointment_use_case: BookPatientAppointmentUseCase
+
 }
 impl AppointmentsController {
-    pub fn with_slots_controller(slots_controller: Arc<Mutex<SlotsController>>) -> AppointmentsController {
+    pub fn with_slots_controller_and_mongo_db(slots_controller: Arc<Mutex<SlotsController>>, mongo_data_base: &MongoDataBase) -> AppointmentsController {
         AppointmentsController {
-            get_available_slots_use_case: GetAvailableSlotsUseCase::with_slots_controller(slots_controller),
+            book_patient_appointment_use_case:BookPatientAppointmentUseCase::with_slots_controller_and_mongo_db(slots_controller.clone(), mongo_data_base),
+            get_available_slots_use_case: GetAvailableSlotsUseCase::with_slots_controller(slots_controller)
         }
     }
 }
@@ -22,5 +27,13 @@ impl AppointmentsController {
     pub async fn get_available_appointments (&self, _patient_id: ObjectId) ->  Result<Vec<AvailableSlotResponse>>  {
         let available_appointments = self.get_available_slots_use_case.invoke().await?;
         Ok(available_appointments)
+    }
+    pub async fn get_patient_booked_appointments (&self, patient_id: ObjectId) ->  Result<Vec<AvailableSlotResponse>>  {
+        let available_appointments = self.get_available_slots_use_case.invoke().await?;
+        Ok(available_appointments)
+    }
+    pub async fn book_appointment (&mut self, patient_id: ObjectId, slot_id: ObjectId ) ->  Result<PatientAppointmentResponse>  {
+        let patient_appointment = self.book_patient_appointment_use_case.invoke(patient_id,slot_id).await?;
+        Ok(patient_appointment)
     }
 }
