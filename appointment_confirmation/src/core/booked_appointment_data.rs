03a4e3bd7475eea725e7fa021283f4192d7963ed
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-01-31 22:05:35 +0000

    Add `BookedAppointmentData` struct to core module
    Introduce a new struct `BookedAppointmentData` to represent booked appointment details, including doctor and patient information, appointment time, and booking timestamp. This serves as a foundational data model for handling appointment confirmation logic.

---
+++ appointment_confirmation/src/core/booked_appointment_data.rs
@@ -0,0 +1,13 @@
+use chrono::{DateTime, Utc};
+
+pub  struct BookedAppointmentData {
+
+    pub doctor_email: String,
+    pub doctor_name: String,
+
+    pub patient_name: String,
+    pub patient_email: String,
+
+    pub appointment_time: DateTime<Utc>,
+    pub booked_at: DateTime<Utc>
+}
