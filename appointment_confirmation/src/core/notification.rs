44d1edc5946876da51e8ea84246177928a1bc1d5
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-01-31 22:06:17 +0000

    Add notification module for appointment confirmations
    Introduced a `Notification` enum and its implementation to generate confirmation texts for both patients and doctors. This improves clarity and centralizes notification logic for booked appointments.

---
+++ appointment_confirmation/src/core/notification.rs
@@ -0,0 +1,21 @@
+use crate::core::BookedAppointmentData;
+pub enum Notification {
+    DoctorAppointmentConfirmationNotification,
+    PatientAppointmentConfirmationNotification,
+}
+
+impl Notification {
+    pub fn get_notification_text(&self, booked_appointment_data: &BookedAppointmentData) -> String {
+        match self {
+            Notification::PatientAppointmentConfirmationNotification => format!(
+                "Hey {},\nYour appointment with Dr. {} at {} has been confirmed.",
+                booked_appointment_data.patient_name, booked_appointment_data.doctor_name, booked_appointment_data.appointment_time),
+
+
+            Notification::DoctorAppointmentConfirmationNotification => format!(
+                "Hey Dr. {},\n{}booked an appointment with you at {}.",
+                booked_appointment_data.doctor_name, booked_appointment_data.patient_name, booked_appointment_data.appointment_time),
+        }
+    }
+}
+
