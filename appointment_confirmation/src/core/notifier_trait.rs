d9134c7a9422e5af6186db277540e55485fdc54d
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-01 16:34:48 +0000

    Add Send and Sync bounds to Notifier trait

--- appointment_confirmation/src/core/notifier_trait.rs
+++ appointment_confirmation/src/core/notifier_trait.rs
@@ -1,6 +1,6 @@
 use async_trait::async_trait;
 use crate::core::BookedAppointmentData;
 #[async_trait]
-pub trait Notifier {
+pub trait Notifier : Send + Sync{
     async fn send_new_booking_notification(&self, booked_appointment_data: BookedAppointmentData);
 }
