3f320ffacdb37cccfa4a59905bc50dbcdce92b34
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-01 16:41:50 +0000

    Add notification trigger to appointment booking usecase
    This update integrates a notifier trigger to send confirmation notifications when a patient appointment is booked. It includes extracting and formatting the appointment details into a payload for the notification system. This enhances communication with patients and doctors post-booking.

--- appointment_booking/src/usecases/book_patient_appointment_use_case.rs
+++ appointment_booking/src/usecases/book_patient_appointment_use_case.rs
@@ -3,15 +3,20 @@ use anyhow::Result;
 use bson::oid::ObjectId;
 use doctor_availability::controllers::SlotsController;
 use futures::lock::Mutex;
-use crate::domain::{AppointmentEntity, PatientAppointmentRepositoryTrait, PatientEntity};
-use crate::infrastructure::{DoctorAvailabilityPatientAppointmentRepository, MongoDataBase, MongoPatientRepository};
+
+use appointment_confirmation::{payloads::AppointmentBookedNotifierPayload, NotifierTrigger};
+use shared::types::ContactData;
+
+use crate::domain::{AppointmentEntity, PatientAppointmentRepositoryTrait};
+use crate::infrastructure::{DoctorAvailabilityPatientAppointmentRepository, MongoDataBase};
 use crate::usecases::GetPatientUseCase;
 use crate::usecases::responses::PatientAppointmentResponse;
 
 
 pub struct BookPatientAppointmentUseCase{
     patient_appointment_repository: Box<dyn PatientAppointmentRepositoryTrait>,
-    get_patient_use_case: GetPatientUseCase
+    get_patient_use_case: GetPatientUseCase,
+    notifier_trigger: NotifierTrigger,
 }
 
 impl BookPatientAppointmentUseCase{
@@ -19,15 +24,28 @@ impl BookPatientAppointmentUseCase{
                                               m_db: &MongoDataBase) -> BookPatientAppointmentUseCase {
         BookPatientAppointmentUseCase {
             patient_appointment_repository: Box::new(DoctorAvailabilityPatientAppointmentRepository::new(slots_controller,m_db)),
-            get_patient_use_case: GetPatientUseCase::with_mongo_db(m_db)
+            get_patient_use_case: GetPatientUseCase::with_mongo_db(m_db),
+            notifier_trigger: NotifierTrigger::new_logging_notifier_trigger()
         }
     }
 }
 
 impl BookPatientAppointmentUseCase {
+    fn extract_appointment_booked_notifier_payload(booked_appointment:&AppointmentEntity) -> AppointmentBookedNotifierPayload{
+       AppointmentBookedNotifierPayload{
+           doctor_contact_data: ContactData::build(booked_appointment.get_doctor_name(), booked_appointment.get_doctor_email()),
+           patient_contact_data: ContactData::build(booked_appointment.get_patient_name(), booked_appointment.get_patient_email()),
+           appointment_time: booked_appointment.get_appointment_start_time().to_chrono(),
+        }
+    }
     pub async fn invoke(&mut self, patient_id: ObjectId, slot_id: ObjectId) -> Result<PatientAppointmentResponse>{
         let patient = self.get_patient_use_case.by_id(patient_id).await?;
         let booked_patient_appointment = self.patient_appointment_repository.create_patient_appointment(patient.to_patient_entity(), slot_id).await?;
+
+        let appointment_booked_notifier_payload = BookPatientAppointmentUseCase::extract_appointment_booked_notifier_payload(&booked_patient_appointment);
+        self.notifier_trigger.appointment_booking_confirmation(appointment_booked_notifier_payload).await;
+
         Ok(PatientAppointmentResponse::from(booked_patient_appointment))
     }
 }
+
