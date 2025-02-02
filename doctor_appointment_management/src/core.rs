c719eea49ab80d0700a96721cd1e27b9e349ba62
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-02 20:13:11 +0000

    Reorganize module imports and update public exports

--- doctor_appointment_management/src/core.rs
+++ doctor_appointment_management/src/core.rs
@@ -1,3 +1,7 @@
-mod appointments_repository_trait;
 mod appointment_entity;
 mod appointment_management_service;
+mod appointments_repository_trait;
+
+pub use appointment_entity::Appointment;
+pub use appointment_management_service::AppointmentManagementService;
+pub use appointments_repository_trait::{AppointmentsRepositoryTrait, AppointmentsRepositoryResult};
