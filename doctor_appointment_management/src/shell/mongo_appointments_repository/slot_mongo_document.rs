e09a2765a4c2122b221f0fef70ae0f1a32f70788
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-02 21:42:33 +0000

    Add SlotMongoDocument to manage slot data for MongoDB integration

---
+++ doctor_appointment_management/src/shell/mongo_appointments_repository/slot_mongo_document.rs
@@ -0,0 +1,10 @@
+use bson::oid::ObjectId;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct SlotMongoDocument {
+    pub _id: ObjectId,
+    pub reserving_patient_id: ObjectId,
+    pub canceled_at: Option<bson::DateTime>,
+    pub completed_at: Option<bson::DateTime>
+}
