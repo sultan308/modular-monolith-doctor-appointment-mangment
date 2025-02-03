c3e524f30f391477ebeb42e2dc58bf17115ab7d7
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-03 20:36:15 +0000

    fix PatientMongoDocument fields.

--- doctor_appointment_management/src/shell/mongo_appointments_repository/patient_mongo_document.rs
+++ doctor_appointment_management/src/shell/mongo_appointments_repository/patient_mongo_document.rs
@@ -4,7 +4,6 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize, Clone)]
 pub struct PatientMongoDocument {
     pub _id: ObjectId,
-    pub reserving_patient_id: ObjectId,
-    pub canceled_at: Option<bson::DateTime>,
-    pub completed_at: Option<bson::DateTime>
+    pub name: String,
+    pub email: String
 }
