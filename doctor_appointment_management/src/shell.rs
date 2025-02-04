f8494d81ce78c6f9ff5d6a7d1a8cf4ad0d2bc972
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-04 19:45:50 +0000

    Add DoctorAppointmentsManagementController implementation

--- doctor_appointment_management/src/shell.rs
+++ doctor_appointment_management/src/shell.rs
@@ -1,1 +1,5 @@
 mod mongo_appointments_repository;
+mod doctor_appointments_management_controller;
+
+pub use doctor_appointments_management_controller::{DoctorAppointmentsManagementController,
+                                                    ResponseAppointment};
---
+++ doctor_appointment_management/src/shell/doctor_appointments_management_controller.rs
@@ -0,0 +1,40 @@
+use anyhow::Result;
+use bson::oid::ObjectId;
+use chrono::{DateTime, Utc};
+use mongodb::Database;
+
+use crate::core::{Appointment, AppointmentManagementService};
+use crate::shell::mongo_appointments_repository::MongoAppointmentsRepository;
+
+mod response_appointment;
+pub use response_appointment::ResponseAppointment;
+
+pub struct DoctorAppointmentsManagementController {
+    appointments_services: AppointmentManagementService
+}
+
+impl DoctorAppointmentsManagementController {
+    pub fn with_mongo_db(mongo_db: &Database) -> DoctorAppointmentsManagementController {
+        DoctorAppointmentsManagementController{
+            appointments_services: AppointmentManagementService::new(Box::new(MongoAppointmentsRepository::new(mongo_db)))
+        }
+    }
+}
+
+impl DoctorAppointmentsManagementController {
+    pub async fn get_all_doctor_appointments(&self, doctor_id: ObjectId, from: Option<DateTime<Utc>>, to: Option<DateTime<Utc>>) -> Result<Vec<ResponseAppointment>> {
+        let from = match from { Some(min) => Some(bson::DateTime::from(min)), _ => None  };
+        let to = match to { Some(max) => Some(bson::DateTime::from(max)), _ => None  };
+        let appointments = self.appointments_services.get_all_doctor_appointments_in_range(doctor_id,from,to).await?;
+        let response_appointments: Vec<ResponseAppointment> = appointments.into_iter().map(ResponseAppointment::from).collect();
+        Ok(response_appointments)
+    }
+    pub async fn cancel_appointment(&self, appointment_id: ObjectId) -> Result<ResponseAppointment> {
+        let canceled_appointment = self.appointments_services.cancel_appointment(appointment_id).await?;
+        Ok(ResponseAppointment::from(canceled_appointment))
+    }
+    pub async fn complete_appointment(&self, appointment_id: ObjectId) -> Result<ResponseAppointment> {
+        let completed_appointment = self.appointments_services.complete_appointment(appointment_id).await?;
+        Ok(ResponseAppointment::from(completed_appointment))
+    }
+}
