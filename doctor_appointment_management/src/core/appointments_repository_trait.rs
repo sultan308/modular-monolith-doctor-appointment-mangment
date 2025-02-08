357600f40b4800d47f4bc26da2b151943cc67856
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-08 22:44:29 +0000

    Update AppointmentsRepositoryTrait result type to use domain-specific error handling

--- doctor_appointment_management/src/core/appointments_repository_trait.rs
+++ doctor_appointment_management/src/core/appointments_repository_trait.rs
@@ -1,9 +1,9 @@
-use anyhow::Result;
 use async_trait::async_trait;
 use bson::{DateTime, oid::ObjectId};
 use crate::core::appointment_entity::Appointment;
+use crate::core::doctor_appointment_management_error::DoctorAppointmentManagementResult;
 
-pub type AppointmentsRepositoryResult<T> = Result<T>;
+
 #[derive(Debug)]
 pub struct AppointmentsFilter {
     pub doctor_id: ObjectId,
@@ -12,7 +12,7 @@ pub struct AppointmentsFilter {
 }
 #[async_trait]
 pub trait AppointmentsRepositoryTrait: Send + Sync {
-    async fn get_appointment(&self, appointment_id: ObjectId) -> AppointmentsRepositoryResult<Appointment>;
-    async fn get_doctor_appointments(&self, filter: AppointmentsFilter) -> AppointmentsRepositoryResult<Vec<Appointment>>;
-    async fn save_appointment_status(&self, appointment: &Appointment) -> AppointmentsRepositoryResult<()>;
+    async fn get_appointment(&self, appointment_id: ObjectId) -> DoctorAppointmentManagementResult<Appointment>;
+    async fn get_doctor_appointments(&self, filter: AppointmentsFilter) -> DoctorAppointmentManagementResult<Vec<Appointment>>;
+    async fn save_appointment_status(&self, appointment: &Appointment) -> DoctorAppointmentManagementResult<()>;
 }
