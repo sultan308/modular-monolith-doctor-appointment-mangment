a8ff4397ea1a631f369587ebe2187cd94c09c09d
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-08 22:45:28 +0000

    Refactor service methods to use DoctorAppointmentManagementError.

--- doctor_appointment_management/src/core/appointment_management_service.rs
+++ doctor_appointment_management/src/core/appointment_management_service.rs
@@ -3,6 +3,7 @@ use bson::DateTime;
 use bson::oid::ObjectId;
 use crate::core::appointment_entity::Appointment;
 use crate::core::appointments_repository_trait::{AppointmentsFilter, AppointmentsRepositoryTrait};
+use crate::core::doctor_appointment_management_error::DoctorAppointmentManagementResult;
 
 pub struct AppointmentManagementService {
     appointments_repository: Box<dyn AppointmentsRepositoryTrait>
@@ -15,20 +16,20 @@ impl AppointmentManagementService {
 }
 
 impl AppointmentManagementService {
-    pub async fn cancel_appointment(&self, appointment_id: ObjectId) -> Result<Appointment> {
+    pub async fn cancel_appointment(&self, appointment_id: ObjectId) -> DoctorAppointmentManagementResult<Appointment> {
         let mut appointment = self.appointments_repository.get_appointment(appointment_id).await?;
-        appointment.canceled();
+        let appointment = appointment.cancel()?;
         self.appointments_repository.save_appointment_status(&appointment).await?;
         Ok(appointment)
     }
-    pub async fn complete_appointment(&self, appointment_id: ObjectId) -> Result<Appointment> {
+    pub async fn complete_appointment(&self, appointment_id: ObjectId) -> DoctorAppointmentManagementResult<Appointment> {
         let mut appointment = self.appointments_repository.get_appointment(appointment_id).await?;
-        appointment.complete();
+        let appointment = appointment.complete()?;
         self.appointments_repository.save_appointment_status(&appointment).await?;
         Ok(appointment)
     }
 
-    pub async fn get_all_doctor_appointments_in_range(&self, doctor_id: ObjectId, from: Option<DateTime>, to: Option<DateTime>) -> Result<Vec<Appointment>> {
+    pub async fn get_all_doctor_appointments_in_range(&self, doctor_id: ObjectId, from: Option<DateTime>, to: Option<DateTime>) -> DoctorAppointmentManagementResult<Vec<Appointment>> {
         let appointments_filter = AppointmentsFilter { doctor_id, from, to };
         let appointment = self.appointments_repository.get_doctor_appointments(appointments_filter).await?;
          Ok(appointment)
