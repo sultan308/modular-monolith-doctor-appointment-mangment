3b0e8ba8e9c5c9918a86805af89ae93f6234e1f3
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-09 17:53:24 +0000

    Refactor error handling in appointment controller methods to use ApplicationResult

--- doctor_appointment_management/src/shell/doctor_appointments_management_controller.rs
+++ doctor_appointment_management/src/shell/doctor_appointments_management_controller.rs
@@ -1,7 +1,7 @@
-use anyhow::Result;
 use bson::oid::ObjectId;
 use chrono::{DateTime, Utc};
 use mongodb::Database;
+use shared::errors::{ApplicationResult, ToApplicationError};
 
 use crate::core::{AppointmentManagementService};
 use crate::shell::mongo_appointments_repository::MongoAppointmentsRepository;
@@ -22,19 +22,22 @@ impl DoctorAppointmentsManagementController {
 }
 
 impl DoctorAppointmentsManagementController {
-    pub async fn get_all_doctor_appointments(&self, doctor_id: ObjectId, from: Option<DateTime<Utc>>, to: Option<DateTime<Utc>>) -> Result<Vec<ResponseAppointment>> {
+    pub async fn get_all_doctor_appointments(&self, doctor_id: ObjectId, from: Option<DateTime<Utc>>, to: Option<DateTime<Utc>>) -> ApplicationResult<Vec<ResponseAppointment>> {
         let from = match from { Some(min) => Some(bson::DateTime::from(min)), _ => None  };
         let to = match to { Some(max) => Some(bson::DateTime::from(max)), _ => None  };
-        let appointments = self.appointments_services.get_all_doctor_appointments_in_range(doctor_id,from,to).await?;
+        let appointments = self.appointments_services.get_all_doctor_appointments_in_range(doctor_id,from,to)
+            .await.map_err(|e| e.to_application_error())?;
         let response_appointments: Vec<ResponseAppointment> = appointments.into_iter().map(ResponseAppointment::from).collect();
         Ok(response_appointments)
     }
-    pub async fn cancel_appointment(&self, appointment_id: ObjectId) -> Result<ResponseAppointment> {
-        let canceled_appointment = self.appointments_services.cancel_appointment(appointment_id).await?;
+    pub async fn cancel_appointment(&self, appointment_id: ObjectId) -> ApplicationResult<ResponseAppointment> {
+        let canceled_appointment = self.appointments_services.cancel_appointment(appointment_id)
+                                               .await.map_err(|e| e.to_application_error())?;
         Ok(ResponseAppointment::from(canceled_appointment))
     }
-    pub async fn complete_appointment(&self, appointment_id: ObjectId) -> Result<ResponseAppointment> {
-        let completed_appointment = self.appointments_services.complete_appointment(appointment_id).await?;
+    pub async fn complete_appointment(&self, appointment_id: ObjectId) -> ApplicationResult<ResponseAppointment> {
+        let completed_appointment = self.appointments_services.complete_appointment(appointment_id)
+                                        .await.map_err(|e| e.to_application_error())?;
         Ok(ResponseAppointment::from(completed_appointment))
     }
 }
