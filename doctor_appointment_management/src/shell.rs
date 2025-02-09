b9f939fa6143c876f169f17f1a2c21292ca81e9f
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-09 17:53:38 +0000

    Add error module for appointment management

--- doctor_appointment_management/src/shell.rs
+++ doctor_appointment_management/src/shell.rs
@@ -1,5 +1,6 @@
 mod mongo_appointments_repository;
 mod doctor_appointments_management_controller;
+mod doctor_appointment_management_error_to_app_error;
 
 pub use doctor_appointments_management_controller::{DoctorAppointmentsManagementController,
                                                     ResponseAppointment};
