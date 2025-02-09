bc689f4640f758554fb5728f2a3ab322548ccb52
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-09 17:52:43 +0000

    Implement ToApplicationError for DoctorAppointmentManagementError properly

--- doctor_appointment_management/src/shell/doctor_appointment_management_error_to_app_error.rs
+++ doctor_appointment_management/src/shell/doctor_appointment_management_error_to_app_error.rs
@@ -4,7 +4,26 @@ use crate::core::DoctorAppointmentManagementError;
 impl ToApplicationError<DoctorAppointmentManagementError> for DoctorAppointmentManagementError {
     fn to_application_error(self: DoctorAppointmentManagementError) -> ApplicationError {
         match self {
-            any => ApplicationError::InternalServerError(Box::new(any))
+            DoctorAppointmentManagementError::AppointmentNotFound(appointment_id) => {
+                ApplicationError::RequestNotFound(format!("An appointment with the requested id of ({appointment_id}) was not found."),
+                                                  Box::new(self))
+            }
+            DoctorAppointmentManagementError::FailedToCancelAppointment(ref appointment,ref reason)  => {
+                ApplicationError::InvalidOperation(format!("Failed to cancel appointment ({}), because {reason}.", appointment.get_id()),
+                                                  Box::new(self))
+            }
+            DoctorAppointmentManagementError::FailedToCompleteAppointment(ref appointment,ref reason)  => {
+                ApplicationError::InvalidOperation(format!("Failed to complete appointment ({}), because {reason}.", appointment.get_id()),
+                                                  Box::new(self))
+            }
+            DoctorAppointmentManagementError::InvalidAppointment(_) |
+            DoctorAppointmentManagementError::FailedToSaveAppointmentStatus(_)|
+            DoctorAppointmentManagementError::PatientNotFound(_)|
+            DoctorAppointmentManagementError::AppointmentsRepositoryError(_)|
+            DoctorAppointmentManagementError::InvalidAppointmentFilter(_,_)|
+            DoctorAppointmentManagementError::InvalidDataReturnedFromSource => {
+                ApplicationError::InternalServerError(Box::new(self))
+            }
         }
     }
 }
