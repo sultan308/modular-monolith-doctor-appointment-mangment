87c3b46fb17f6d480c56befc000489f1be04bc8d
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-10 22:34:27 +0000

    Refactor repository error handling in slots repository

--- doctor_availability/src/data/repositories/slots_mongo_repository.rs
+++ doctor_availability/src/data/repositories/slots_mongo_repository.rs
@@ -2,71 +2,96 @@ use async_trait::async_trait;
 use bson::{doc, Document, oid::ObjectId};
 use futures::TryStreamExt;
 use mongodb::{Collection, Database};
-use std::sync::Arc;
 
-use crate::data::repositories::{SlotsRepository, SlotsRepositoryResult, SlotsRepositoryFilter};
 use crate::data::data_models::SlotDataModel;
+
+use crate::data::repositories::slots_repository_trait::{SlotsRepository, SlotsRepositoryFilter};
+use crate::data::repositories::repository_error::{RepositoryResult,RepositoryError};
+
 const DEFAULT_SLOTS_COLLECTION_NAME: &str = "slots";
 
 
 pub struct SlotsMongoRepository {
-    slots_collection: Arc<Collection<SlotDataModel>>
+    slots_collection: Collection<SlotDataModel>
 }
 impl SlotsMongoRepository {
     pub fn new(mongo_db: &Database) -> SlotsMongoRepository {
         SlotsMongoRepository::with_collection_name(mongo_db, DEFAULT_SLOTS_COLLECTION_NAME)
     }
-    pub fn with_collection_name(mongo_db: &Database, doctors_collection_name: &str) -> SlotsMongoRepository {
-        let doctors_collection: Collection<SlotDataModel> = mongo_db.collection(doctors_collection_name);
-        SlotsMongoRepository {slots_collection: Arc::from(doctors_collection) }
+    pub fn with_collection_name(mongo_db: &Database, slots_collection_name: &str) -> SlotsMongoRepository {
+        let slots_collection: Collection<SlotDataModel> = mongo_db.collection(slots_collection_name);
+        SlotsMongoRepository {slots_collection }
     }
 }
 
 #[async_trait]
 impl SlotsRepository for SlotsMongoRepository {
 
-    async fn delete(&mut self, slot_id: ObjectId, doctor_id: ObjectId) -> SlotsRepositoryResult<()> {
+    async fn delete(&mut self, slot_id: ObjectId, doctor_id: ObjectId) -> RepositoryResult<()> {
 
-        self.slots_collection
+        let delete_result = self.slots_collection
             .delete_one(doc! { "_id": slot_id , "doctor_id": doctor_id })
-            .await?;
+            .await
+            .map_err(|e| RepositoryError::InternalRepositoryError(Box::new(e)))?;
+
+        if delete_result.deleted_count != 1 {
+            return Err(RepositoryError::ItemNotFoundError(slot_id));
+        }
         Ok(())
     }
 
-    async fn list(&self, slot_filter: SlotsRepositoryFilter) -> SlotsRepositoryResult<Vec<SlotDataModel>> {
-        let filter =slot_filter.to_mongo_filter_document();
-        let cursor = self.slots_collection.find(filter).await?;
-        let slots = cursor.try_collect().await?;
+    async fn list(&self, slot_filter: SlotsRepositoryFilter) -> RepositoryResult<Vec<SlotDataModel>> {
+        let filter =slot_filter.to_mongo_filter_document(doc!{});
+
+        let cursor = self.slots_collection.find(filter).await
+            .map_err(|e| RepositoryError::InternalRepositoryError(Box::new(e)))?;
+        let slots = cursor.try_collect().await
+            .map_err(|e| RepositoryError::InternalRepositoryError(Box::new(e)))?;
+
         Ok(slots)
     }
 
-    async fn list_doctor_slots(&self, doctor_id: ObjectId) -> SlotsRepositoryResult<Vec<SlotDataModel>> {
-        let cursor = self.slots_collection.find(doc! {"doctor_id": doctor_id}).await?;
-        let slots = cursor.try_collect().await?;
+    async fn list_doctor_slots(&self, doctor_id: ObjectId) -> RepositoryResult<Vec<SlotDataModel>> {
+        let cursor = self.slots_collection
+            .find(doc! {"doctor_id": doctor_id})
+            .await
+            .map_err(|e| RepositoryError::InternalRepositoryError(Box::new(e)))?;
+
+        let slots = cursor.try_collect().await
+            .map_err(|e| RepositoryError::InternalRepositoryError(Box::new(e)))?;
+
         Ok(slots)
     }
 
-    async fn load(&self,slot_id: ObjectId) -> SlotsRepositoryResult<Option<SlotDataModel>> {
+    async fn load(&self,slot_id: ObjectId) -> RepositoryResult<SlotDataModel> {
         let result = self
             .slots_collection
             .find_one(doc! { "_id": slot_id })
-            .await?;
-        Ok(result)
+            .await
+            .map_err(|e| RepositoryError::InternalRepositoryError(Box::new(e)))?;
+        result.ok_or(RepositoryError::ItemNotFoundError(slot_id))
     }
 
-    async fn create(&mut self, slot_data: &SlotDataModel) -> SlotsRepositoryResult<ObjectId> {
-        self.slots_collection.insert_one(slot_data).await?;
+    async fn create(&mut self, slot_data: &SlotDataModel) -> RepositoryResult<ObjectId> {
+        self.slots_collection.insert_one(slot_data).await
+            .map_err(|e| RepositoryError::InternalRepositoryError(Box::new(e)))?;
         Ok(slot_data._id)
     }
 
-    async fn update(&mut self, slot_data: &SlotDataModel, filter: Option<SlotsRepositoryFilter>) -> SlotsRepositoryResult<()> {
+    async fn update(&mut self, slot_data: &SlotDataModel, filter: Option<SlotsRepositoryFilter>) -> RepositoryResult<()> {
+        let filter = filter.unwrap_or_else(|| SlotsRepositoryFilter::new());
+        let mongo_filter = filter.to_mongo_filter_document(doc! { "_id": slot_data._id});
 
-        let filter = match filter {
-            Some(filter) => filter.to_mongo_filter_document(),
-            None => doc! { "_id": slot_data._id}
-        };
-        let update = doc! { "$set": bson::to_document(slot_data)? };
-        self.slots_collection.update_one(filter, update).await?;
+        let bson_updated_slot = bson::to_document(slot_data)
+            .map_err(|e| RepositoryError::InternalRepositoryError(Box::new(e)))?;
+
+        let update = doc! { "$set": bson_updated_slot};
+        let update_result = self.slots_collection.update_one(mongo_filter, update).await
+            .map_err(|e| RepositoryError::InternalRepositoryError(Box::new(e)))?;
+
+        if update_result.matched_count != 1 {
+            return Err(RepositoryError::ItemNotFoundError(slot_data._id))
+        }
         Ok(())
     }
 }
@@ -92,8 +117,8 @@ impl SlotsRepositoryFilter {
             None => None
         }
     }
-    pub fn to_mongo_filter_document(self) -> Document {
-        let mut filter = doc!();
+    pub fn to_mongo_filter_document(self, base_filter: Document) -> Document {
+        let mut filter = base_filter;
         let reserved_at_filter = SlotsRepositoryFilter::does_exist_filter(self.is_reserved);
         let completed_at_filter = SlotsRepositoryFilter::does_exist_filter(self.is_reserved);
         let canceled_at_filter = SlotsRepositoryFilter::does_exist_filter(self.is_reserved);
--- doctor_availability/src/data/repositories/slots_repository_trait.rs
+++ doctor_availability/src/data/repositories/slots_repository_trait.rs
@@ -1,12 +1,11 @@
-use anyhow::Result;
 use async_trait::async_trait;
 use bson::{DateTime,doc,oid::ObjectId};
 use serde::{Deserialize, Serialize};
 use serde_with::{skip_serializing_none};
 
 use crate::data::data_models::SlotDataModel;
+use crate::data::repositories::repository_error::RepositoryResult;
 
-pub type SlotsRepositoryResult<T> = Result<T>;
 
 #[skip_serializing_none]
 #[derive(Debug, Deserialize, Serialize, Clone)]
@@ -19,13 +18,28 @@ pub struct SlotsRepositoryFilter {
     pub time_before: Option<DateTime>,
     pub time_after: Option<DateTime>
 }
+impl SlotsRepositoryFilter {
+    pub fn new() -> SlotsRepositoryFilter {
+        SlotsRepositoryFilter{
+             doctor_id: None,
+             patient_id: None,
+             is_reserved: None,
+             is_canceled: None,
+             is_completed: None,
+             time_before: None,
+             time_after: None
+
+        }
+    }
+
+}
 
 #[async_trait]
 pub trait SlotsRepository: Sync + Send {
-    async fn delete(&mut self,slot_id: ObjectId, doctor_id: ObjectId) -> SlotsRepositoryResult<()>;
-    async fn list(&self, slot_filter: SlotsRepositoryFilter) -> SlotsRepositoryResult<Vec<SlotDataModel>>;
-    async fn list_doctor_slots(&self, doctor_id: ObjectId) -> SlotsRepositoryResult<Vec<SlotDataModel>>;
-    async fn load(&self, slot_id: ObjectId) -> SlotsRepositoryResult<Option<SlotDataModel>>;
-    async fn create(&mut self, slot_data: &SlotDataModel) -> SlotsRepositoryResult<ObjectId>;
-    async fn update(&mut self, slot_data: &SlotDataModel, filter: Option<SlotsRepositoryFilter>) -> SlotsRepositoryResult<()>;
+    async fn delete(&mut self,slot_id: ObjectId, doctor_id: ObjectId) -> RepositoryResult<()>;
+    async fn list(&self, slot_filter: SlotsRepositoryFilter) -> RepositoryResult<Vec<SlotDataModel>>;
+    async fn list_doctor_slots(&self, doctor_id: ObjectId) -> RepositoryResult<Vec<SlotDataModel>>;
+    async fn load(&self, slot_id: ObjectId) -> RepositoryResult<SlotDataModel>;
+    async fn create(&mut self, slot_data: &SlotDataModel) -> RepositoryResult<ObjectId>;
+    async fn update(&mut self, slot_data: &SlotDataModel, filter: Option<SlotsRepositoryFilter>) -> RepositoryResult<()>;
 }
