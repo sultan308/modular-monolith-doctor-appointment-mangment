742d27f185b3c076811227d2d208185325ca6ed4
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-10 22:34:41 +0000

    Update repository exports for consistency

--- doctor_availability/src/data/repositories.rs
+++ doctor_availability/src/data/repositories.rs
@@ -4,8 +4,10 @@ mod slots_mongo_repository;
 mod slots_repository_trait;
 mod repository_error;
 
-pub use doctors_repository_trait::{DoctorsRepository, DoctorsRepositoryResult};
-pub use slots_repository_trait::{SlotsRepository, SlotsRepositoryResult, SlotsRepositoryFilter};
+pub use doctors_repository_trait::{DoctorsRepository};
+pub use slots_repository_trait::{SlotsRepository, SlotsRepositoryFilter};
 
-pub use doctors_mongo_repository::{DoctorsMongoRepository};
-pub use slots_mongo_repository::{SlotsMongoRepository};
+pub use doctors_mongo_repository::DoctorsMongoRepository;
+pub use slots_mongo_repository::SlotsMongoRepository;
+
+pub use repository_error::{RepositoryError,RepositoryResult};
