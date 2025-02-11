c9851a935e7fb6f3e5760023af8b5b943b724552
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-11 21:52:49 +0000

    Add DoctorAvailabilityError for DoctorAvailability business layer

---
+++ doctor_availability/src/business/doctor_availability_error.rs
@@ -0,0 +1,45 @@
+
+use bson::oid::ObjectId;
+use std::{error, fmt};
+
+use crate::business::models::Slot;
+
+pub type DoctorAvailabilityResult<T> = Result<T, DoctorAvailabilityError>;
+#[derive(Debug)]
+
+pub enum DoctorAvailabilityError {
+    FailedToAddSlot(String),
+    FailedReserveSlot(Slot, String),
+    FailedDeleteSlot(Slot, String),
+    SlotNotFound(ObjectId),
+    DoctorNotFound(ObjectId),
+    InternalError(Box<dyn error::Error + Send + Sync>),
+}
+
+impl fmt::Display for DoctorAvailabilityError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            DoctorAvailabilityError::FailedToAddSlot(reason) => {
+                write!(f, "DoctorAvailabilityError::FailedToAddSlot: Couldn't add slot because {reason}.")
+            }
+            DoctorAvailabilityError::FailedReserveSlot(_, reason) => {
+                write!(f, "DoctorAvailabilityError::FailedReserveSlot: Couldn't reserve slot because {reason}.")
+            }
+            DoctorAvailabilityError::FailedDeleteSlot(_, reason) => {
+                write!(f, "DoctorAvailabilityError::FailedDeleteSlot: Couldn't delete slot because {reason}.")
+            }
+            DoctorAvailabilityError::SlotNotFound(not_found_slot_id) => {
+                write!(f, "DoctorAvailabilityError::SlotNotFound: Couldn't find a slot with id of {not_found_slot_id}.")
+            }
+            DoctorAvailabilityError::DoctorNotFound(not_found_doctor_id) => {
+                write!(f, "DoctorAvailabilityError::DoctorNotFound: Couldn't find a doctor with id of {not_found_doctor_id}.")
+
+            }
+            DoctorAvailabilityError::InternalError(internal_err) => {
+                write!(f, "DoctorAvailabilityError::InternalError: {internal_err}.")
+            }
+        }
+    }
+}
+
+impl error::Error  for DoctorAvailabilityError {}
