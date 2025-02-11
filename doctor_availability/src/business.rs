b19780d09497ba7e082a587ce3cf3cb9f8fe63b4
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-11 21:47:58 +0000

    Add DoctorAvailabilityError for DoctorAvailability business layer

--- doctor_availability/src/business.rs
+++ doctor_availability/src/business.rs
@@ -1,5 +1,7 @@
 mod services;
 mod models;
+mod doctor_availability_error;
 
+pub use doctor_availability_error::{DoctorAvailabilityError,DoctorAvailabilityResult};
 pub use models::{Slot, Doctor};
 pub use services::{DoctorServices,SlotsServices};
