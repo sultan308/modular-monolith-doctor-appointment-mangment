b7abec41c592626c500e647be746a6e2bcf83660
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-16 21:25:48 +0000

    Add appointment_booking_error module to appointment_booking domain

---
+++ appointment_booking/src/domain/appointment_booking_error.rs
@@ -0,0 +1,29 @@
+use bson::oid::ObjectId;
+use std::{error, fmt};
+use crate::domain::AppointmentEntity;
+
+pub type AppointmentBookingResult<T> = Result<T, AppointmentBookingError>;
+
+#[derive(Debug)]
+pub enum AppointmentBookingError {
+    AppointmentNotFound(ObjectId),
+    AppointmentAlreadyBooked(AppointmentEntity),
+    DoctorNotFound(ObjectId),
+    InternalBookingError(Box<dyn error::Error>),
+    PatientNotFound(ObjectId),
+
+}
+
+impl fmt::Display for AppointmentBookingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AppointmentBookingError::AppointmentNotFound(appointment_id) => write!(f, "AppointmentBookingError::AppointmentNotFound: Couldn't find an appointment with id of {appointment_id}"),
+            AppointmentBookingError::AppointmentAlreadyBooked(appointment) => write!(f, "AppointmentBookingError::AppointmentAlreadyBooked: appointment ({}) couldn't be booked because it's already booked",appointment.get_id()),
+            AppointmentBookingError::DoctorNotFound(doctor_id) => write!(f, "AppointmentBookingError::DoctorNotFound: Doctor with id of ({doctor_id}) was not found."),
+            AppointmentBookingError::InternalBookingError( internal_err) => write!(f, "AppointmentBookingError::InternalBookingError: {}", internal_err.to_string()),
+            AppointmentBookingError::PatientNotFound(patient_id) => write!(f, "AppointmentBookingError::PatientNotFound: Patient with id of ({patient_id}) was not found."),
+        }
+    }
+}
+
+impl error::Error  for AppointmentBookingError {}
