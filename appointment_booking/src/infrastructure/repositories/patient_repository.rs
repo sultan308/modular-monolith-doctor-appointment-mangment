b9f87b84ecaff7b6e108097eddc240efc5059b76
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-18 23:16:34 +0000

    Update MongoPatientRepository to reflect new PatientRepositoryTrait changes

--- appointment_booking/src/infrastructure/repositories/patient_repository.rs
+++ appointment_booking/src/infrastructure/repositories/patient_repository.rs
@@ -1,18 +1,18 @@
 use async_trait::async_trait;
 use bson::{oid::ObjectId, doc};
 use mongodb::{Collection, Database};
-use crate::domain::{PatientEntity, PatientRepositoryResult, PatientRepositoryTrait};
 
+use crate::domain::{AppointmentBookingError, AppointmentBookingResult,
+                    PatientEntity, PatientRepositoryTrait};
 use crate::infrastructure::data_models::PatientDataModel;
-const DEFAULT_SLOTS_COLLECTION_NAME: &str = "patients";
-
+const DEFAULT_PATIENTS_COLLECTION_NAME: &str = "patients";
 
 pub struct MongoPatientRepository {
     patients_collection: Collection<PatientDataModel>
 }
 impl MongoPatientRepository {
     pub fn new(mongo_db: &Database) -> MongoPatientRepository {
-        MongoPatientRepository::with_collection_name(mongo_db, DEFAULT_SLOTS_COLLECTION_NAME)
+        MongoPatientRepository::with_collection_name(mongo_db, DEFAULT_PATIENTS_COLLECTION_NAME)
     }
     pub fn with_collection_name(mongo_db: &Database, doctors_collection_name: &str) -> MongoPatientRepository {
         let patients_collection: Collection<PatientDataModel> = mongo_db.collection(doctors_collection_name);
@@ -21,19 +21,23 @@ impl MongoPatientRepository {
 }
 #[async_trait]
 impl PatientRepositoryTrait for MongoPatientRepository{
-    async fn get_patient_by_id(&self, patient_id: ObjectId) -> PatientRepositoryResult<PatientEntity> {
+    async fn get_patient_by_id(&self, patient_id: ObjectId) -> AppointmentBookingResult<PatientEntity> {
         let target_patient = self
             .patients_collection
             .find_one(doc! { "_id": patient_id })
-            .await?;
-        Ok(target_patient.unwrap().to_domain_entity())
+            .await
+            .map_err(|mongo_err| AppointmentBookingError::InternalBookingError(Box::new(mongo_err)))?;
+        let patient_model = target_patient.ok_or(AppointmentBookingError::PatientNotFound(patient_id))?;
+        Ok(patient_model.to_domain_entity())
     }
 
-    async fn create_patient(&mut self, new_patient: &PatientEntity) -> PatientRepositoryResult<ObjectId> {
+    async fn create_patient(&mut self, new_patient: &PatientEntity) -> AppointmentBookingResult<ObjectId> {
         let new_patient_data = PatientDataModel::from(new_patient);
         let patient_create_res = self
             .patients_collection
-            .insert_one(new_patient_data).await?;
-        Ok(patient_create_res.inserted_id.as_object_id().unwrap())
+            .insert_one(new_patient_data)
+            .await
+            .map_err(|mongo_err| AppointmentBookingError::InternalBookingError(Box::new(mongo_err)))?;;
+        Ok(new_patient.get_id())
     }
 }
