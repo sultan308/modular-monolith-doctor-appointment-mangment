54152f7f17060d2ca011b165c3f48f90d144f69f
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-19 19:50:31 +0000

    Refactor error handling in CreatePatientUseCase using ApplicationError

--- appointment_booking/src/usecases/create_patient_use_case.rs
+++ appointment_booking/src/usecases/create_patient_use_case.rs
@@ -1,6 +1,6 @@
-use anyhow::Result;
+use shared::errors::{ApplicationError,ApplicationResult};
 
-use crate::domain::{PatientEntity,PatientRepositoryTrait};
+use crate::domain::{AppointmentBookingError, PatientEntity, PatientRepositoryTrait};
 use crate::infrastructure::{MongoDataBase,MongoPatientRepository};
 use crate::usecases::responses::ResponsePatient;
 pub struct CreatePatientUseCaseInterface {
@@ -21,12 +21,14 @@ impl CreatePatientUseCase {
 }
 
 impl CreatePatientUseCase{
-    pub async fn invoke(&mut self, create_patient_use_case_interface: CreatePatientUseCaseInterface) -> Result<ResponsePatient>{
+    pub async fn invoke(&mut self, create_patient_use_case_interface: CreatePatientUseCaseInterface) -> ApplicationResult<ResponsePatient>{
         let patient_entity = PatientEntity::new(
             &create_patient_use_case_interface.name,
             &create_patient_use_case_interface.email
         );
-        self.patients_repo.create_patient(&patient_entity).await?;
+        self.patients_repo.create_patient(&patient_entity)
+            .await
+            .map_err(|appointment_booking_error: AppointmentBookingError| ApplicationError::InternalServerError(Box::new(appointment_booking_error)))?;
         Ok(ResponsePatient::from(patient_entity))
     }
 }
