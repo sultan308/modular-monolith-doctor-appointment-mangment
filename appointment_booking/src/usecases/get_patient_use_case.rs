ea3e3673223797c0a1709d3e73919ab4acd58ed7
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-19 19:36:25 +0000

    Refactor error handling in GetPatientUseCase to us ApplicationResult

--- appointment_booking/src/usecases/get_patient_use_case.rs
+++ appointment_booking/src/usecases/get_patient_use_case.rs
@@ -1,7 +1,7 @@
-use anyhow::Result;
 use bson::{oid::ObjectId};
+use shared::errors::{ApplicationError,ApplicationResult};
 
-use crate::domain::PatientRepositoryTrait;
+use crate::domain::{PatientRepositoryTrait,AppointmentBookingError};
 use crate::infrastructure::{MongoDataBase,MongoPatientRepository};
 use crate::usecases::responses::ResponsePatient;
 
@@ -18,8 +18,15 @@ impl GetPatientUseCase {
 }
 
 impl GetPatientUseCase{
-    pub async fn by_id(&self, patient_id: ObjectId) -> Result<ResponsePatient>{
-        let retrieved_patient = self.patients_repo.get_patient_by_id(patient_id).await?;
+    pub async fn by_id(&self, patient_id: ObjectId) -> ApplicationResult<ResponsePatient>{
+        let retrieved_patient = self.patients_repo.get_patient_by_id(patient_id)
+            .await
+            .map_err(|appointment_booking_error:AppointmentBookingError| {
+                if let AppointmentBookingError::PatientNotFound(patient_id) = appointment_booking_error {
+                    return ApplicationError::RequestNotFound(format!("Patient with requested id ({patient_id}) is not found"), Box::new(appointment_booking_error));
+                };
+                ApplicationError::InternalServerError(Box::new(appointment_booking_error))
+            })?;
         Ok(ResponsePatient::from(retrieved_patient))
     }
 }
