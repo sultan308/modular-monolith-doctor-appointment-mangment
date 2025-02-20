2435e21a81e1bd1bf6b5464b87572677c9b438df
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-20 22:30:30 +0000

    Refactor router patient handlers to use `ApplicationResult`.

--- src/routers/patient_routers.rs
+++ src/routers/patient_routers.rs
@@ -1,9 +1,11 @@
 use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::{Json, Router};
+use axum::extract::rejection::JsonRejection;
 use axum::routing::{get, post};
 
 use mongodb::bson::{oid::ObjectId};
+use shared::errors::{ApplicationResult, ApplicationError};
 
 use appointment_booking::payloads::patient_payloads::CreatePatientPayload;
 use appointment_booking::responses::{AvailableSlotResponse, ResponsePatient, PatientAppointmentResponse};
@@ -22,43 +24,43 @@ pub fn get_patients_router(app_state: AppState) -> Router {
 }
 
 async fn create_patient_handler(State(app_state): State<AppState>,
-                                   Json(create_patient_payload): Json<CreatePatientPayload>) -> (StatusCode, Json<ResponsePatient>){
-
+                                   create_patient_payload: Result<Json<CreatePatientPayload>,JsonRejection>) -> ApplicationResult<(StatusCode, Json<ResponsePatient>)>{
+        let Json(create_patient_payload) = create_patient_payload.map_err(|e| ApplicationError::InvalidRequestPayload("Invalid create patient body".to_string(), Box::new(e)))?;
         let mut patients_controller = app_state.patient_controllers.lock().await;
-        let response_patient = patients_controller.create_patient(create_patient_payload).await.unwrap();
-        (StatusCode::CREATED, Json::from(response_patient))
+        let response_patient = patients_controller.create_patient(create_patient_payload).await?;
+        Ok((StatusCode::CREATED, Json::from(response_patient)))
 }
 
 async fn get_patient_by_id_handler(State(app_state): State<AppState>,
-                                Path(patient_id): Path<ObjectId>) -> (StatusCode, Json<ResponsePatient>){
+                                Path(patient_id): Path<ObjectId>) -> ApplicationResult<(StatusCode, Json<ResponsePatient>)>{
 
     let mut patients_controller = app_state.patient_controllers.lock().await;
-    let response_patient = patients_controller.get_patient(patient_id).await.unwrap();
-    (StatusCode::OK, Json::from(response_patient))
+    let response_patient = patients_controller.get_patient(patient_id).await?;
+    Ok((StatusCode::OK, Json::from(response_patient)))
 }
 async fn get_patient_appointments(State(app_state): State<AppState>,
-                                   Path(patient_id): Path<ObjectId>) -> (StatusCode, Json<Vec<PatientAppointmentResponse>>){
+                                   Path(patient_id): Path<ObjectId>) -> ApplicationResult<(StatusCode, Json<Vec<PatientAppointmentResponse>>)>{
 
     let appointments_controller = app_state.appointments_controller.lock().await;
-    let patient_appointments = appointments_controller.get_patient_booked_appointments(patient_id).await.unwrap();
-    (StatusCode::OK, Json::from(patient_appointments))
+    let patient_appointments = appointments_controller.get_patient_booked_appointments(patient_id).await?;
+    Ok((StatusCode::OK, Json::from(patient_appointments)))
 }
 
 async fn get_available_appointments(State(app_state): State<AppState>,
-                                   Path(patient_id): Path<ObjectId>) -> (StatusCode, Json<Vec<AvailableSlotResponse>>){
+                                   Path(patient_id): Path<ObjectId>) -> ApplicationResult<(StatusCode, Json<Vec<AvailableSlotResponse>>)>{
 
     let appointments_controller = app_state.appointments_controller.lock().await;
-    let response_patient = appointments_controller.get_available_appointments(patient_id).await.unwrap();
-    (StatusCode::OK, Json::from(response_patient))
+    let response_patient = appointments_controller.get_available_appointments(patient_id).await?;
+    Ok((StatusCode::OK, Json::from(response_patient)))
 }
 
 async fn book_patient_appointment(State(app_state): State<AppState>,
                                    Path((patient_id, slot_id)): Path<(ObjectId, ObjectId)>
-                                    ) -> (StatusCode, Json<PatientAppointmentResponse>){
+                                    ) -> ApplicationResult<(StatusCode, Json<PatientAppointmentResponse>)>{
 
     let mut appointments_controller = app_state.appointments_controller.lock().await;
-    let response_booked_patient_appointment = appointments_controller.book_appointment(patient_id, slot_id).await.unwrap();
-    (StatusCode::CREATED, Json::from(response_booked_patient_appointment))
+    let response_booked_patient_appointment = appointments_controller.book_appointment(patient_id, slot_id).await?;
+    Ok((StatusCode::CREATED, Json::from(response_booked_patient_appointment)))
 }
 
 
