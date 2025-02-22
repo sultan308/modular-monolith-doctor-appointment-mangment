801272c65961449b80a03fd11c821f5460e76154
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2025-02-22 22:23:19 +0000

    fix email regex handling.

--- shared/src/types/email.rs
+++ shared/src/types/email.rs
@@ -1,14 +1,15 @@
 use std::fmt::Display;
 use regex::Regex;
 use crate::errors::{ApplicationError, ApplicationResult};
-const EMAIL_REGEX: Regex =  Regex::new(r#"(?i)^(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)*$"#).unwrap();
+const EMAIL_REGEX_STR: &str =  r#"(?i)^(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)*$"#;
 
 pub struct Email {
     address: String,
 }
 impl Email{
     fn validate_address(address: &str) -> ApplicationResult<()>{
-        if EMAIL_REGEX.is_match(address) {return Ok(())};
+        let email_regex = Regex::new(EMAIL_REGEX_STR).unwrap();
+        if email_regex.is_match(address) {return Ok(())};
         Err(ApplicationError::InvalidEmail(address.to_string()))
     }
 }
